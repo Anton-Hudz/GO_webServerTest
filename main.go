fc8546ae27d46d2037cb13610dcbb4e26894a177
Anton Hudz <pat280885@mail.ru>
2022-09-05 15:51:19 +0300

    add first stage of my task

--- main.go
+++ main.go
@@ -1,44 +1,37 @@
 package main
 
 import (
-	// "bufio"
-	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
-	// "os"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
-	// ______________________________________________
-	// reader := bufio.NewReader(os.Stdin)
-	// for {
-	// 	fmt.Println("What is your name?")
-	// 	text, _ := reader.ReadString('\n')
-	// 	fmt.Println("Hello", text)
-	// }
-	//___________________________________________
+	var (
+		minInt int
+		maxInt int
+	)
 
-	http.HandleFunc("/", mainPage)
-	http.HandleFunc("/1", mainPage1)
+	fmt.Println("Type Min integer")
+	fmt.Fscan(os.Stdin, &minInt)
+	fmt.Println("Type Max integer")
+	fmt.Fscan(os.Stdin, &maxInt)
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	randomNumber := strconv.Itoa(minInt + rand.Intn(maxInt-minInt+1))
+	fmt.Println("Your random number is:", randomNumber)
+
+	http.HandleFunc("/random", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(randomNumber))
+	})
 
 	port := ":8080"
 	fmt.Println("Server listen on port:", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
-}
-
-type User struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-}
-
-func mainPage(w http.ResponseWriter, r *http.Request) {
-	user := User{"Vasya", "Ivanov"}
-	js, _ := json.Marshal(user)
-	w.Write(js)
-}
-func mainPage1(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("It is amazing"))
 }
